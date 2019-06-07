6e15cab2710b80baba4d604712a9c274321f3a95
Jason Farrell <jason@useallfive.com>
2019-06-07 07:14:38 -0700

    Initial animation mvp.

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+    },
+    plugins: ['lit', 'prettier'],
+    extends: ['plugin:lit/recommended'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    rules: {
+        semi: ['warn', 'never'],
+        'object-curly-spacing': ['error', 'always'],
+        'require-jsdoc': 'off',
+        // 'indent': ['error', 4],
+        'max-len': [
+            'error',
+            120,
+            {
+                ignoreTemplateLiterals: true,
+            },
+        ],
+        'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+        'no-underscore-dangle': ['error', { allowAfterThis: true }],
+        'prettier/prettier': 'warn',
+    },
+}
