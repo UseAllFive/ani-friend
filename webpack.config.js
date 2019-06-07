3fe96e67f616b194c6b1a729bde01fed5bc8c383
Jason Farrell <jason@useallfive.com>
2019-06-07 08:01:06 -0700

    Basic webpack setup.

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+const commonConfig = {
+    context: __dirname + '/src',
+    entry: ['@babel/polyfill', './index.js'],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'Ani.js',
+    },
+    resolve: {
+        modules: ['node_modules', path.resolve(__dirname, './src/index.js')],
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new CopyWebpackPlugin([
+            {
+                from: __dirname + '/src/images/**/*',
+                to: __dirname + '/dist/',
+            },
+        ]),
+        new HtmlWebpackPlugin({
+            title: 'Ani Examples',
+            template: __dirname + '/src/index.html',
+            inject: 'body',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    targets: {
+                                        esmodules: true,
+                                    },
+                                },
+                            ],
+                        ],
+                    },
+                },
+            },
+        ],
+    },
+}
+
+module.exports = [commonConfig]
